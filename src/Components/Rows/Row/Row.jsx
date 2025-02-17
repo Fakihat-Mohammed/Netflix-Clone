5c7d5ca7679928bd612e473f5181befd98fe5686
Fakihat-Mohammed <fakihmoh44@gmail.com>
2025-02-17 12:00:43 +0300

    building rows

---
+++ src/Components/Rows/Row/Row.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Row.module.css";
+import axios from "../../../utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+import { height, width } from "@mui/system";
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrialerUrl] = useState("");
+  const base_url = "https://image.tmdb.org/t/p/original";
+  useEffect(() => {
+    (async () => {
+      try {
+        // console.log(fetchUrl);
+        const request = await axios.get(fetchUrl);
+        // console.log(request);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [fetchUrl]);
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrialerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          console.log(urlParams.get("v"));
+          setTrialerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  return (
+    <div className={styles.row}>
+      <h1>{title}</h1>
+      <div className={styles.row_posters}>
+        {movies?.map((movie, index) => {
+          return (
+            <img
+              onClick={() => handleClick(movie)}
+              key={index}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`${styles.row_poster} ${
+                isLargeRow && styles.row_posterLarge
+              }`}
+            />
+          );
+        })}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
