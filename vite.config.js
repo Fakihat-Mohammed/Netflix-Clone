43de9469dbc68ee35008df00215b9d1142fed0d3
Fakihat-Mohammed <fakihmoh44@gmail.com>
2025-02-18 07:23:40 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Netflix-Clone/",
+  base: "/Netflix-Clone/",
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", 
+  },
+});
