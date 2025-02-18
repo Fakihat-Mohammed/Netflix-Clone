d0287bbbea36019b3cd6fe3bcad0e387e2309f29
Fakihat-Mohammed <fakihmoh44@gmail.com>
2025-02-18 07:31:37 +0300

    add 1 row

--- src/Components/Rows/RowList/RowList.jsx
+++ src/Components/Rows/RowList/RowList.jsx
@@ -11,13 +11,13 @@ const RowList = () => {
         isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRatedMovies} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />{" "}
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-
     </>
   );
 }
